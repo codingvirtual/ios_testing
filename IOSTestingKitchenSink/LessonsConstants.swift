13ce07771b0635a840e3f7e633039a031b043af9
Grant Kemp <grant@kempnet.com>
2015-09-07 23:26:19 +0100

    Initial skeleton and idea for lesson 1

---
+++ IOSTestingKitchenSink/LessonsConstants.swift
@@ -0,0 +1,33 @@
+//
+//  LessonsConstants.swift
+//  IOSTestingKitchenSink
+//
+//  Created by Grant Kemp on 07/09/2015.
+//  Copyright (c) 2015 Grant Kemp. All rights reserved.
+//
+
+import Foundation
+
+class LessonsConstants {
+    
+    let numberOfLessons = 1
+    var lesson = [Int:String]()
+    
+    init() {
+        // Based on http://osherove.com/tdd-kata-1/
+        lesson[0] = "String Calculator"
+    }
+    
+    // MARK: - Shared Instance
+    
+    class func sharedInstance() -> LessonsConstants {
+        
+        struct Singleton {
+            static var sharedInstance = LessonsConstants()
+        }
+        
+        return Singleton.sharedInstance
+    }
+}
+
+    
