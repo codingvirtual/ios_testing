21fe7be7e5e2d94d825e1efa0d765878917412ba
Grant Kemp <grant@kempnet.com>
2015-09-07 22:54:44 +0100

    Initial Commit

---
+++ IOSTestingKitchenSink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSTestingKitchenSink
+//
+//  Created by Grant Kemp on 07/09/2015.
+//  Copyright (c) 2015 Grant Kemp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
